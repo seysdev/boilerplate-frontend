fad2a49df3378f01c386102c42cd789f489a2327
sebastian yabiku <sby2710@gmail.com>
2024-05-23 21:10:39 +0200

    feat(base): add base project

---
+++ .releaserc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  branches: ['main'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    "@semantic-release/npm",
+    [
+      "@semantic-release/changelog",
+      {
+        "changelogFile": "CHANGELOG.md"
+      }
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json', 'CHANGELOG.md'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+    [
+      "@semantic-release/github",
+      {
+        "assets": [{"path": "CHANGELOG.md"}]
+      }
+    ],
+  ],
+};
